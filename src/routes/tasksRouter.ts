5016ee723ab79d33c44520fc18027a67c601308c
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-05-18 17:20:49 -0300

    feat: creates post route

--- src/routes/tasksRouter.ts
+++ src/routes/tasksRouter.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import TasksController from '../controllers/tasksController';
+import tasksValidation from '../middlewares/tasksValidation';
 
 const router = Router();
 
@@ -12,5 +13,7 @@ router.get('/:id', tasksController.getById);
 router.put('/:id', tasksValidation, tasksController.update);
 
 router.delete('/:id', tasksController.delete);
+
+router.post('/', tasksValidation, tasksController.create);
 
 export default router;
