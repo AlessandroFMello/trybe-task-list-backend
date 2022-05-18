23660eb1afce9670c7b1948759ae225ff12346a1
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-05-18 17:12:25 -0300

    feat: implements getAll function in tasksServices

--- src/services/tasksServices.ts
+++ src/services/tasksServices.ts
@@ -2,4 +2,12 @@ import { StatusCodes } from 'http-status-codes';
 import prisma from '../models/connection';
 
 export default class TasksServices {
+  public async getAll() {
+    const tasks = await prisma.taskList.findMany();
+
+    if (!tasks) return { code: StatusCodes.NOT_FOUND, message: 'Task not found' };
+
+    return { code: StatusCodes.OK, tasks };
+  }
+
 }
